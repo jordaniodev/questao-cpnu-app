e68ba9ff7cefb0964508302dc7c1ad992bbf1c5a
luigiBilhon <luigi.jordanio@bilhon.tech>
2025-08-15 22:44:28 -0300

    feat: add Clarity analytics script to layout for improved user insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -105,6 +105,15 @@ export default function RootLayout({
               gtag('config', 'G-KC1CFFB8FS');
             `}
           </Script>
+          <Script id="clarity-analytics" strategy="afterInteractive">
+            {`
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "svhx2ik86g");
+            `}
+          </Script>
         </body>
       </html>
     </ClerkProvider>
